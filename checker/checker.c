4043ae8a8c1adda83f6bacb09318a77db28dae34
BooDeer <hsissi3a@gmail.com>
2021-07-06 19:30:14 +0100

    added norm

--- checker/checker.c
+++ checker/checker.c
@@ -6,17 +6,16 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 15:35:55 by hboudhir          #+#    #+#             */
-/*   Updated: 2021/07/06 18:55:33 by hboudhir         ###   ########.fr       */
+/*   Updated: 2021/07/06 19:29:57 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pushswap.h"
 
-
 int	check_action(char *a)
 {
 	int	len;
-	
+
 	len = ft_strlen(a);
 	if (len == 2)
 	{
@@ -33,27 +32,9 @@ int	check_action(char *a)
 	return (1);
 }
 
-void	exec_instruction(t_struct *stack_a, t_struct *stack_b, char *buff)
+static void	exec_instruction2(t_struct *stack_a, t_struct *stack_b, char *buff)
 {
-	if (!strcmp(buff, "sa"))
-		swap_stack(&(stack_a->stack), stack_a->length);
-	else if (!strcmp(buff, "sb"))
-		swap_stack(&(stack_b->stack), stack_b->length);
-	else if (!strcmp(buff, "ss"))
-	{
-		swap_stack(&(stack_a->stack), stack_a->length);
-		swap_stack(&(stack_b->stack), stack_b->length);
-
-	}
-	else if (!strcmp(buff, "pa"))
-		push_stack(stack_b, stack_a);
-	else if (!strcmp(buff, "pb"))
-		push_stack(stack_a, stack_b);
-	else if (!strcmp(buff, "ra"))
-		rotate_stack(stack_a->stack, stack_a->length);
-	else if (!strcmp(buff, "rb"))
-		rotate_stack(stack_b->stack, stack_b->length);
-	else if (!strcmp(buff, "rr"))
+	if (!strcmp(buff, "rr"))
 	{
 		rotate_stack(stack_a->stack, stack_a->length);
 		rotate_stack(stack_b->stack, stack_b->length);
@@ -67,38 +48,68 @@ void	exec_instruction(t_struct *stack_a, t_struct *stack_b, char *buff)
 		revrotate_stack(stack_a->stack, stack_a->length);
 		revrotate_stack(stack_b->stack, stack_b->length);
 	}
-	
 }
 
-int		main(int ac, char **av)
+void	exec_instruction(t_struct *stack_a, t_struct *stack_b, char *buff)
 {
-	char	*buff;
-	int		rt;
-	t_struct stack_a;
-	t_struct stack_b;
-	
+	if (!strcmp(buff, "sa"))
+		swap_stack(&(stack_a->stack), stack_a->length);
+	else if (!strcmp(buff, "sb"))
+		swap_stack(&(stack_b->stack), stack_b->length);
+	else if (!strcmp(buff, "ss"))
+	{
+		swap_stack(&(stack_a->stack), stack_a->length);
+		swap_stack(&(stack_b->stack), stack_b->length);
+	}
+	else if (!strcmp(buff, "pa"))
+		push_stack(stack_b, stack_a);
+	else if (!strcmp(buff, "pb"))
+		push_stack(stack_a, stack_b);
+	else if (!strcmp(buff, "ra"))
+		rotate_stack(stack_a->stack, stack_a->length);
+	else if (!strcmp(buff, "rb"))
+		rotate_stack(stack_b->stack, stack_b->length);
+	exec_instruction2(stack_a, stack_b, buff);
+}
+
+void	init_stack(t_struct *stack_a, t_struct *stack_b, int ac, char **av)
+{
+	stack_a->length = ac - 1;
+	stack_a->arr_length = stack_a->length;
+	stack_b->length = 0;
+	ft_fill_list(&(stack_a->stack), stack_a->length, (av + 1));
+	ft_fill_list(&(stack_b->stack), 0, 0);
+}
+
+void	ft_read_line(int *rt, char **buff, t_struct *stack_a, t_struct *stack_b)
+{
+	*rt = get_next_line(0, buff);
+	if (check_action(*buff) && *rt)
+	{	
+		free(*buff);
+		free(stack_b->stack);
+		free(stack_a->stack);
+		exit_msg(2, "Error\n", 1);
+	}
+	else
+		exec_instruction(stack_a, stack_b, *buff);
+}
+
+int	main(int ac, char **av)
+{
+	char		*buff;
+	int			rt;
+	t_struct	stack_a;
+	t_struct	stack_b;
+
 	if (ac < 2)
-		return (0) ;
+		return (0);
 	if (nbr_only(av + 1))
 		exit_msg(2, "Error\n", 1);
-	stack_a.length = ac -1;
-	stack_a.arr_length = stack_a.length;
-	stack_b.length = 0;
-	ft_fill_list(&(stack_a.stack), stack_a.length, (av + 1));
-	ft_fill_list(&(stack_b.stack), 0, 0);
-	
+	init_stack(&stack_a, &stack_b, ac, av);
 	while (1)
 	{
-		rt = get_next_line(0, &buff);
-		if(check_action(buff) && rt)
-		{	
-			free(buff);
-			free(stack_b.stack);
-			free(stack_a.stack);
-			exit_msg(2, "Error\n", 1);
-		}
-		else
-			exec_instruction(&stack_a, &stack_b, buff);
+		ft_read_line(&rt, &buff, &stack_a, &stack_b);
 		if (!rt)
 		{
 			if (check_order(stack_a.stack, stack_a.length))
@@ -108,6 +119,6 @@ int		main(int ac, char **av)
 			break ;
 		}
 	}
-	write(1, "OK\n", ft_strlen("OK\n")); 
+	write(1, "OK\n", ft_strlen("OK\n"));
 	return (0);
 }
